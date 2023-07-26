31e1e61349c67eb247681711de3830ff66ca40d2
Dejan Vasic <dejanvasic24@gmail.com>
2023-07-26 22:41:13 +1000

    Removing orange colour

--- src/lib/colours.ts
+++ src/lib/colours.ts
@@ -3,7 +3,6 @@ export const colours = [
 	{ name: 'indigo', cssClass: 'bg-indigo-800' },
 	{ name: 'blue', cssClass: 'bg-blue-800' },
 	{ name: 'rose', cssClass: 'bg-rose-800' },
-	{ name: 'orange', cssClass: 'bg-orange-800' },
 	{ name: 'amber', cssClass: 'bg-amber-800' },
 	{ name: 'teal', cssClass: 'bg-teal-800' }
 ];
