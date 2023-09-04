766cdf397bc617c512bea98fbd71535d344852c2
Dejan Vasic <dejanvasic24@gmail.com>
2023-09-04 22:43:26 +1000

    Removing the temporary code for the board

--- src/routes/board/+page.svelte
+++ src/routes/board/+page.svelte
@@ -1,35 +1,4 @@
 <script lang="ts">
-	import { withAuth } from '$lib/auth';
-
-	const auth = withAuth();
-	$: user = auth.user;
-	$: accessToken = auth.accessToken;
-
-	interface Hello {
-		hello: string;
-	}
-
-	async function getBoard(): Promise<Hello | null> {
-		if (!$accessToken) return null;
-		const res = await fetch('/api/board', {
-			cache: 'no-cache',
-			headers: { Authorization: `Bearer ${$accessToken}` }
-		});
-
-		if (res.ok) {
-			const data = await res.json();
-			return data as Hello;
-		}
-		throw new Error(res.statusText);
-	}
 </script>
 
-<div>User: {JSON.stringify($user, null, 2)}</div>
-
-{#await getBoard()}
-	<p>Loading ...</p>
-{:then data}
-	<p>{JSON.stringify(data?.hello || {}, null, 2)}</p>
-{:catch error}
-	<p>{error.message}</p>
-{/await}
+<div>Coming soon ... brace yourself</div>
