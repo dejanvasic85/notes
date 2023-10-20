9954c06f9b34e18bff9a943bcb3acf0c6502441c
Dejan Vasic <dejanvasic24@gmail.com>
2023-10-20 23:34:20 +1100

    Formatting

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ const config: PlaywrightTestConfig = {
 		port: 4173
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
 
 export default config;
