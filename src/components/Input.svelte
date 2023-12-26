410bd8ff4d4983daf741b6d746569f881795f0b5
Dejan Vasic <dejanvasic24@gmail.com>
2023-12-26 22:43:52 +1100

    Removing console log

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -4,6 +4,7 @@
 
 	import { twMerge } from 'tailwind-merge';
 
+	// Props
 	export let focusOnMount = false;
 	export let type: HTMLInputTypeAttribute = 'text';
 	export let value: any = undefined;
@@ -16,7 +17,6 @@
 		}
 	});
 
-	console.log('restProps', $$restProps);
 	const defaultClass =
 		'rounded-md border border-gray-300 bg-gray-50 p-2 text-gray-900 dark:border-gray-600 dark:bg-slate-800 dark:text-white dark:placeholder-gray-400';
 	$: inputClass = twMerge(defaultClass, $$restProps.class);
