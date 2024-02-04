dc2f7dd4e0f45614325a2a3ebdc2b39e4be6a6e2
Dejan Vasic <dejanvasic24@gmail.com>
2024-02-04 20:38:26 +1100

    Console log in the db

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { fieldEncryptionExtension } from 'prisma-field-encryption';
 
+console.log('PRISMA_FIELD_ENCRYPTION_KEY', process.env.PRISMA_FIELD_ENCRYPTION_KEY);
 const prisma = new PrismaClient().$extends(fieldEncryptionExtension());
 
 export default prisma;
