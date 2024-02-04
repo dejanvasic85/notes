b98fea52d7918090361edb33808f434f7ce3e5ed
Dejan Vasic <dejanvasic24@gmail.com>
2024-02-04 22:33:33 +1100

    Removing the console log

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { fieldEncryptionExtension } from 'prisma-field-encryption';
 
-console.log('PRISMA_FIELD_ENCRYPTION_KEY', process.env.PRISMA_FIELD_ENCRYPTION_KEY, process.env.DATABASE_URL);
 const prisma = new PrismaClient().$extends(fieldEncryptionExtension());
 
 export default prisma;
