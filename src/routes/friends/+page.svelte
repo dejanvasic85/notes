9d7418a0a8719f7e86b2954be67ed514179dc887
Dejan Vasic <dejanvasic24@gmail.com>
2024-02-22 22:01:00 +1100

    Adding a basic form for adding a friend non functional

---
+++ src/routes/friends/+page.svelte
@@ -0,0 +1,8 @@
+<div class="container mx-auto max-w-4xl">
+	<h1 class="text-2xl">Share your notes with family and friends</h1>
+
+	<div>
+		<h2 class="text-xl">Invites</h2>
+		<a href="/friends/invite" class="underline">Invite collaborator</a>
+	</div>
+</div>
