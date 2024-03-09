0b3b211339d59c7e53c3fe292fd95d3a03ee1593
Dejan Vasic <dejanvasic24@gmail.com>
2024-03-09 23:03:06 +1100

    Removing all prerendering

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,6 @@
 import type { LayoutServerLoad } from './$types';
 
-export const prerender = true;
+export const prerender = false;
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	return {
