7c2cd78a59cc0545a95e03924e4458433c926f16
Dejan Vasic <dejanvasic24@gmail.com>
2024-03-12 18:01:42 +1100

    Removing the auth header handler from hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,44 +1,11 @@
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-import { either as E, taskEither as TE } from 'fp-ts';
+import { taskEither as TE } from 'fp-ts';
 import { pipe } from 'fp-ts/lib/function';
 
 import { matchesPrivateRoute } from '$lib/auth/privateRoutes';
-import { verifyToken } from '$lib/auth/verifyToken';
 import { getSession, setAuthCookie } from '$lib/auth/session';
-import { getOrCreateUserByAuth } from '$lib/server/services/userService';
-
-const getTokenFromHeader = (authHeader: string) => authHeader.split(' ')[1];
-
-const handleAuthHeader: Handle = async ({ event, resolve }) => {
-	if (event.locals.user) {
-		// Todo: this hanler is temporary so remove it once everything is moved to session
-		return await resolve(event);
-	}
-
-	const authHeader = event.request.headers.get('Authorization');
-	if (authHeader) {
-		const accessToken = getTokenFromHeader(authHeader);
-		const decodedToken = await verifyToken<{ sub: string }>(accessToken);
-		if (!decodedToken) {
-			throw new Error('Invalid token');
-		}
-
-		const result = await pipe(
-			getOrCreateUserByAuth({ accessToken, authId: decodedToken.sub }),
-			TE.map((user) => {
-				event.locals.user = user;
-			})
-		)();
-
-		if (E.isLeft(result)) {
-			throw new Error('Failed to get or create user');
-		}
-	}
-
-	return await resolve(event);
-};
 
 const handleSessionCookie: Handle = async ({ event, resolve }) => {
 	return await pipe(
@@ -63,4 +30,4 @@ const handleSessionCookie: Handle = async ({ event, resolve }) => {
 	)();
 };
 
-export const handle = sequence(handleSessionCookie, handleAuthHeader);
+export const handle = sequence(handleSessionCookie);
