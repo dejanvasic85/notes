c7e2402e5268300f0ffc735a5d5c78057d4d0601
Dejan Vasic <dejanvasic24@gmail.com>
2024-10-05 21:16:12 +1000

    Adding heading to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Button from '$components/Button.svelte';
 	import logo from '$lib/images/notes-main.png';
 </script>
 
@@ -7,10 +8,17 @@
 	<meta name="description" content="Personal note taking application and sharing with family" />
 </svelte:head>
 
-<header class="container mx-auto h-20 border border-white">
-	<nav>
+<header class="borderpx-6 container mx-auto h-20">
+	<nav class="flex w-full items-center justify-between">
 		<img src={logo} alt="Notes" class="size-20" />
+		<Button>Login</Button>
 	</nav>
 </header>
 
-<section class="flex w-full flex-col items-center justify-center"></section>
+<section class="flex w-full flex-[0.6] flex-col items-center justify-center">
+	<h1 class="text-6xl">
+		Take Notes
+		<span class="block font-bold text-primary">Privately</span>
+	</h1>
+	<p class="mt-8 text-lg">Your Notes, Your Data, Your Peace of Mind</p>
+</section>
