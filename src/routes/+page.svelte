3a9a09f5fd6004cce530feb40d9b18319fd7f33d
Dejan Vasic <dejanvasic24@gmail.com>
2024-10-12 21:36:08 +1100

    Replacing playground with the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,108 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import partition from 'lodash/partition';
+
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
 	import LinkButton from '$components/LinkButton.svelte';
+	import Board from '$components/Board.svelte';
+
 	import logo from '$lib/images/notes-main.png';
 	import github from '$lib/images/github.svg';
+	import { generateId } from '$lib/identityGenerator';
+
+	import { reorderNotes } from '$lib/notes';
+	import type { Note } from '$lib/types';
+
+	import { localBoard, orderedNotes } from '$lib/noteStore';
+
+	onMount(() => {
+		const id = generateId('nid');
+		localBoard.update(() => ({
+			id: generateId('bid'),
+			userId: '',
+			noteOrder: [id],
+			notes: [
+				{
+					id,
+					text: 'Use the force and edit me by clicking here.',
+					textPlain: 'Use the force and edit me by clicking here.',
+					boardId: $localBoard.id!,
+					colour: 'blue'
+				}
+			]
+		}));
+	});
+
+	function handleCreateNote() {
+		const id = generateId('nid');
+		localBoard.update((current) => {
+			return {
+				...current,
+				noteOrder: [...current.noteOrder, id],
+				notes: [
+					...current.notes,
+					{ id, text: `New note`, textPlain: `New note`, boardId: $localBoard.id!, colour: null }
+				]
+			};
+		});
+
+		goto(`/?id=${id}`);
+	}
+
+	function handleUpdateNote({ detail: { note } }: CustomEvent<{ note: Note }>) {
+		localBoard.update((state) => {
+			const [[noteToUpdate], rest] = partition(state.notes, (n) => n.id === note.id);
+			console.log('noteToUpdate', noteToUpdate, state);
+			return {
+				...state,
+				noteOrder: [...state.noteOrder],
+				notes: [...rest, { ...note }]
+			};
+		});
+	}
+
+	function handleDeleteNote({ detail }: CustomEvent<{ note: Note }>) {
+		localBoard.update((state) => {
+			return {
+				...state,
+				noteOrder: [...state.noteOrder.filter((id) => id !== detail.note.id)],
+				notes: [...state.notes.filter((n) => n.id !== detail.note.id)]
+			};
+		});
+		handleClose();
+	}
+
+	function handleClose() {
+		goto('/');
+	}
+
+	function handleSelect({ detail: { id } }: CustomEvent<{ id: string }>) {
+		goto(`/?id=${id}`);
+	}
+
+	function handleReorder({
+		detail: { fromIndex, toIndex }
+	}: CustomEvent<{ fromIndex: number; toIndex: number }>) {
+		localBoard.update((state) => {
+			const newOrder = reorderNotes(state.noteOrder, fromIndex, toIndex);
+			return {
+				...state,
+				noteOrder: newOrder
+			};
+		});
+	}
+
+	$: search = new URL($page.url).searchParams;
+	$: selectedId = search.get('id');
+	$: selectedNote = $orderedNotes.find((n) => n.id === selectedId);
 </script>
 
 <svelte:head>
 	<title>My Notes - personal note taking application</title>
-	<meta name="description" content="Personal note taking application and sharing with family" />
+	<meta name="description" content="A simple note taking application." />
 </svelte:head>
 
 <header class="borderpx-6 container mx-auto h-20 border-b-2">
@@ -20,7 +116,7 @@
 	</nav>
 </header>
 
-<section class="hero-bg flex flex-[0.6] flex-col items-center justify-center gap-8 rounded-xl">
+<section class="flex flex-col items-center justify-center gap-8 rounded-xl pt-20 sm:pt-24 lg:pt-32">
 	<h1 class="text-6xl md:text-8xl">
 		Take Notes
 		<span class="block font-bold text-primary">Privately</span>
@@ -35,8 +131,22 @@
 		<LinkButton href="/api/auth/login?screen_hint=register" variant="tertiary"
 			>Get started</LinkButton
 		>
-		<LinkButton href="/playground" variant="secondary">Login</LinkButton>
+		<LinkButton href="/api/auth/login" variant="secondary">Login</LinkButton>
 	</div>
 
 	<p class="text-center italic">Easily create notes and share them with your family and friends.</p>
 </section>
+
+<div class="mt-8">
+	<Board
+		notes={$orderedNotes}
+		{selectedNote}
+		selectedSharedNote={null}
+		on:createNote={handleCreateNote}
+		on:closeNote={handleClose}
+		on:deleteNote={handleDeleteNote}
+		on:reorder={handleReorder}
+		on:select={handleSelect}
+		on:updateNote={handleUpdateNote}
+	/>
+</div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -7,12 +7,11 @@
 
 <header class="container mx-auto px-4">
 	<nav class="flex justify-center space-x-4 pt-4">
-		<HeaderLink label="Playground" path="/playground" />
 		{#if isAuthenticated}
 			<HeaderLink label="My Board" path="/my/board" />
 			<HeaderLink label="Friends" path="/my/friends" />
 			<HeaderLink label="Logout" path="/api/auth/logout" />
-			<div class="px-3 py-2 inline-block">Hello <span class="font-bold">{name}</span></div>
+			<div class="inline-block px-3 py-2">Hello <span class="font-bold">{name}</span></div>
 		{:else}
 			<HeaderLink label="Login" path="/api/auth/login" />
 		{/if}
