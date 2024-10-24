628e38453367c8cdfaff1580d45673fde8da2ac8
Dejan Vasic <dejanvasic24@gmail.com>
2024-10-24 21:53:55 +1100

    Updating homepage

--- src/components/LinkButton.svelte
+++ src/components/LinkButton.svelte
@@ -5,7 +5,7 @@
 
 <a
 	{href}
-	class="rounded-lg border-2 border-{variant} px-6 py-3 transition-colors duration-300 dark:text-white hover:bg-{variant}/90 md:text-lg"
+	class="rounded-lg border-2 border-{variant} px-6 py-3 transition-all duration-300 hover:scale-105 dark:text-white hover:bg-{variant}/90 md:text-lg"
 >
 	<slot />
 </a>
