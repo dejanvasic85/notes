70585004c8bec149452eb4b6b9582df9b629e27b
Dejan Vasic <dejanvasic24@gmail.com>
2024-12-01 18:41:04 +1100

    Simplifying the main layout to allow for fluid layout in the dashboard

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,33 +1,16 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	export let fullWidth = false;
+	type Props = {
+		fullWidth?: boolean;
+		children: any;
+	};
 
-	let scrolled = false;
-	const handlerScroll = () => (scrolled = window.scrollY > 10);
-
-	onMount(() => {
-		window.addEventListener('scroll', handlerScroll);
-
-		return () => {
-			window.removeEventListener('scroll', handlerScroll);
-		};
-	});
-
-	$: scrolledClass = scrolled ? 'header-bg' : 'bg-transparent';
+	let { children, fullWidth = false }: Props = $props();
 </script>
 
 <header
 	class="z-50 h-16 {fullWidth
 		? 'w-full'
-		: 'container'} sticky top-0 mx-auto border-b-2 bg-background p-2 px-4 dark:bg-dark {scrolledClass} "
+		: 'container'} mx-auto border-b-2 bg-background p-2 px-4 dark:bg-dark"
 >
-	<slot />
+	{@render children()}
 </header>
-
-<style>
-	.header-bg {
-		flex: 0 0 auto;
-		box-shadow: 1px 1px 10px #0006;
-		backdrop-filter: blur(100px);
-	}
-</style>
