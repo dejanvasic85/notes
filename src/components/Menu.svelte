bbb8f144ca6f11afdd87e2c90b07f1a18d15a296
Dejan Vasic <dejanvasic24@gmail.com>
2025-01-05 23:09:00 +1100

    Adding a nice effect when clicking on a menu icon

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Icon from './Icon.svelte';
 	import Button from './Button.svelte';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 
 	type Props = {
 		oncreatenote: () => void;
@@ -9,7 +9,15 @@
 	};
 
 	let { oncreatenote, layout }: Props = $props();
+	let iconPress = $state<null | 'home' | 'friends'>(null);
 	const iconSize = layout === 'horizontal' ? 36 : 32;
+
+	function handleIconPress(name: 'home' | 'friends') {
+		iconPress = name;
+		setTimeout(() => {
+			iconPress = null;
+		}, 300);
+	}
 </script>
 
 <nav
@@ -17,11 +25,17 @@
 		? 'justify-around'
 		: 'flex-col justify-center'}"
 >
-	<a href="/my/board" aria-label="My board" class="rounded-xl px-4 py-2 hover:dark:bg-darkHover">
+	<a
+		href="/my/board"
+		aria-label="My board"
+		class="rounded-xl px-4 py-2 transition-all hover:dark:bg-darkHover"
+		class:pressed={iconPress === 'home'}
+		onclick={() => handleIconPress('home')}
+	>
 		<Icon
 			icon="home"
 			size={iconSize}
-			fill={$page.url.pathname === '/my/board' ? 'currentColor' : 'none'}
+			fill={page.url.pathname === '/my/board' ? 'currentColor' : 'none'}
 		/>
 	</a>
 	<Button onclick={oncreatenote} variant="primary">
@@ -31,11 +45,31 @@
 		href="/my/friends"
 		aria-label="My friends"
 		class="rounded-xl px-4 py-2 hover:dark:bg-darkHover"
+		class:pressed={iconPress === 'friends'}
+		onclick={() => handleIconPress('friends')}
 	>
 		<Icon
 			icon="users"
 			size={iconSize}
-			fill={$page.url.pathname === '/my/friends' ? 'currentColor' : 'none'}
+			fill={page.url.pathname === '/my/friends' ? 'currentColor' : 'none'}
 		/>
 	</a>
 </nav>
+
+<style>
+	@keyframes shrink {
+		0% {
+			transform: scale(1);
+		}
+		50% {
+			transform: scale(0.8); /* Adjust the scale value to control how small the icon gets */
+		}
+		100% {
+			transform: scale(1);
+		}
+	}
+
+	.pressed {
+		animation: shrink 0.3s ease; /* Adjust duration and easing to your preference */
+	}
+</style>
