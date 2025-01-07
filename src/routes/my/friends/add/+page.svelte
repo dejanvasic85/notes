c6e1fa903cd86a37d847210cfc5050db83926ac9
Dejan Vasic <dejanvasic24@gmail.com>
2025-01-07 22:16:32 +1100

    Enhancing the form with handling loading state

--- src/routes/my/friends/add/+page.svelte
+++ src/routes/my/friends/add/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import { createLabel, melt } from '@melt-ui/svelte';
 
 	import Input from '$components/Input.svelte';
 	import Button from '$components/Button.svelte';
 	import Icon from '$components/Icon.svelte';
 
-	let { form } = $props();
+	let loading = $state(false);
+	let error = $state('');
+
 	const {
 		elements: { root }
 	} = createLabel();
-	const formError = form?.status === 400;
 </script>
 
 <div class="mb-4">
@@ -20,17 +22,28 @@
 
 <h1 class="text-2xl">Invite friend</h1>
 <p>Adding a friend allows you select notes for them to see.</p>
-<form method="POST">
+<form
+	method="POST"
+	use:enhance={() => {
+		loading = true;
+		return async ({ result }) => {
+			loading = false;
+			if (result.type === 'failure') {
+				error = result.data?.message as string;
+			}
+		};
+	}}
+>
 	<div class="mt-4 flex w-full flex-col gap-2 lg:w-1/2">
-		<label for="email" class:text-error={formError} use:melt={$root}>Email</label>
+		<label for="email" class:text-error={!!error} use:melt={$root}>Email</label>
 		<div class="flex-1">
-			<Input id="email" type="text" invalid={formError} name="email" />
+			<Input id="email" type="text" invalid={!!error} name="email" />
 		</div>
-		{#if formError}
-			<p class="mt-2 text-sm text-error">Email is required</p>
+		{#if error}
+			<p class="mt-2 text-sm text-error">{error}</p>
 		{/if}
 		<div class="flex justify-end">
-			<Button type="submit">Send invite</Button>
+			<Button type="submit" {loading}>Send invite</Button>
 		</div>
 	</div>
 </form>
