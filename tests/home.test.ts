76e913408b4e48057418b2da56a6a5739555d4c4
Dejan Vasic <dejanvasic24@gmail.com>
2025-10-17 22:28:40 +1100

    Adding some timeouts

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -5,6 +5,7 @@ test('edit sample note', async ({ page }) => {
 	await expect(
 		page.getByText('Click here to edit me! You can also drag to reorder 😊')
 	).toBeVisible();
+	await page.waitForTimeout(1000);
 	await page.getByRole('button', { name: 'Edit note 1' }).click();
 	await page.waitForTimeout(1000);
 });
@@ -14,6 +15,8 @@ test('add a new note', async ({ page }) => {
 	await page.getByRole('button', { name: 'Create a note' }).click();
 	await page.waitForTimeout(1000);
 	await page.keyboard.type(' hello world');
+	await page.waitForTimeout(1000);
 	await page.getByRole('button', { name: 'Save note' }).click();
+	await page.waitForTimeout(1000);
 	await expect(page.getByText('hello world')).toBeVisible();
 });
